74c92a4f09cc99bd56418454ef8718e4625fbe90
mayoayodele <38493112+mayoayodele@users.noreply.github.com>
2020-03-18 17:36:27 +0000

    new generated files

--- Problem/Parameters.py
+++ Problem/Parameters.py
@@ -3,33 +3,33 @@ class Parameters:
     def __init__(self):
         self.period = 28
         self.number_of_problems = 20
-        all_parameters = {'Client': {'Actual': 31,
+        all_parameters = {'Client': {'Actual': 30, #31
                             'Small': 10,
                             'Medium': 20,
                             'Large': 30},
-                'FE': {'Actual': 96,
+                'FE': {'Actual': 100, #96
                             'Small': 40,
                             'Medium': 70,
                             'Large': 100},
-                'FE_Per_Client': {'Actual': [1,26],
-                            'Small': [1,10],
-                            'Medium': [11,20],
-                            'Large': [21,30]},
-                'Capacity_Per_FE': {'Actual': [1,47],
+                'FE_Per_Client': {'Actual': [1,27], #[1,26],
+                            'Small': [1,9],
+                            'Medium': [10,18],
+                            'Large': [19,27]},
+                'Capacity_Per_FE': {'Actual': [1,45], #[1,47],
                             'Small': [1,15],
                             'Medium': [16,30],
                             'Large': [31,45]},
-                'Lifecycle': {'Actual': [10,300],
-                            'Small': [10,90],
-                            'Medium': [91,170],
-                            'Large': [171,250]},
-                'Jobs_Per_Client': {'Actual': [1,95],
+                'Lifecycle': {'Actual': [11, 250],#[10,300],
+                            'Small': [11,100],
+                            'Medium': [101,190],
+                            'Large': [191,280]},
+                'Jobs_Per_Client': {'Actual': [1,96], #[1,95],
                             'Small': [1,32],
                             'Medium': [33,64],
                             'Large': [65,96]},
                 'Utilisation': {'Actual': [0,1],
                             'Small': [0,0.33],
-                            'Medium': [0.34,0.67],
+                            'Medium': [0.34,0.66],
                             'Large': [0.67,1]}
             }
 
@@ -49,7 +49,7 @@ class Parameters:
 
         folder_name = 'Actual'
         medium_params = {parameter: all_parameters[parameter]['Actual'] for parameter in list(all_parameters.keys())}
-        folder_names_and_parameters = {folder_name: medium_params}
+        folder_names_and_parameters[folder_name] = medium_params
                 
 
         self.folder_names_and_parameters = folder_names_and_parameters
